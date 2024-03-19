409fc248ba77cc676e49c2494458ad280c61cd1e
Lucas Vieira <lucasltv@gmail.com>
2024-03-18 21:49:59 -0300

    Add comments, posts, and users schema files

---
+++ src/drizzle/schema/posts.ts
@@ -0,0 +1,20 @@
+import { relations } from 'drizzle-orm';
+import { integer, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
+import { comments } from './comments';
+import { users } from './users';
+
+export const posts = pgTable('posts', {
+  id: serial('id').primaryKey(),
+  title: text('title').notNull(),
+  content: text('content').notNull(),
+  userId: integer('user_id')
+    .notNull()
+    .references(() => users.id),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
+});
+
+export const postsRelations = relations(posts, ({ one, many }) => ({
+  user: one(users, { fields: [posts.userId], references: [users.id] }),
+  comments: many(comments),
+}));
--- src/drizzle/schema/users.ts
+++ src/drizzle/schema/users.ts
@@ -1,7 +1,15 @@
-import { pgTable, serial, text } from 'drizzle-orm/pg-core';
+import { relations } from 'drizzle-orm';
+import { pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
+import { posts } from './posts';
 
-export const users = pgTable('user', {
+export const users = pgTable('users', {
   id: serial('id').primaryKey(),
-  email: text('email').primaryKey(),
-  // role_id: integer('role_id'),
+  name: text('name').notNull(),
+  email: text('email').notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
 });
+
+export const usersRelations = relations(users, ({ many }) => ({
+  posts: many(posts),
+}));
